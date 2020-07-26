cdb614cb8f2a21e9c6b7ee372812627a597bd7be
Sahn Lam <slam@users.noreply.github.com>
2020-07-25 17:49:50 -0700

    Create python-app.yml (#1)

--- brokers/test_tdameritrade.py
+++ brokers/test_tdameritrade.py
@@ -1,8 +1,7 @@
 import pytest
-import oauthlib
 from .tdameritrade import TdaPortfolio
 
 
 def test_bad_login():
-    with pytest.raises(oauthlib.oauth2.rfc6749.errors.InvalidClientError):
+    with pytest.raises(Exception):
         TdaPortfolio("123", "abc", "def", "https://localhost")
