6bfd57077cad7778cf67b94724e00e6770535f0d
Sahn Lam <slam@users.noreply.github.com>
2020-07-29 19:04:04 -0700

    Add generic buy and sell stop cmds instead

--- src/slamtrader/brokers/tdameritrade.py
+++ src/slamtrader/brokers/tdameritrade.py
@@ -1,11 +1,126 @@
 import json
 
 from tda import auth
-from tda.orders.common import Duration, one_cancels_other, OrderType
-from tda.orders.equities import equity_sell_limit, equity_sell_market
+from tda.orders.common import Duration, OrderType
+from tda.orders.equities import equity_sell_market
+from tda.utils import Utils
 
 
-class TdaPortfolio:
+class BrokerException(Exception):
+    def __init__(self, response):
+        j = response.json()
+        if "error" not in j:
+            self.message = json.dumps(j, indent=4)
+        self.message = response.json()["error"]
+        super().__init__(self.message)
+
+
+class Position:
+    def __init__(self, raw):
+        # {
+        #     "shortQuantity": 0.0,
+        #     "averagePrice": 399.95,
+        #     "currentDayProfitLoss": 659.59,
+        #     "currentDayProfitLossPercentage": 2.22,
+        #     "longQuantity": 71.0,
+        #     "settledLongQuantity": 71.0,
+        #     "settledShortQuantity": 0.0,
+        #     "instrument": {
+        #         "assetType": "EQUITY",
+        #         "cusip": "252131107",
+        #         "symbol": "DXCM"
+        #     },
+        #     "marketValue": 30322.68,
+        #     "maintenanceRequirement": 30322.68
+        # }
+        self.raw = raw
+
+    @property
+    def symbol(self):
+        return self.raw["instrument"]["symbol"]
+
+    @property
+    def long(self):
+        return self.raw["longQuantity"]
+
+    @property
+    def short(self):
+        return self.raw["shortQuantity"]
+
+    @property
+    def trade_price(self):
+        return self.raw["averagePrice"]
+
+
+class Order:
+    def __init__(self, raw):
+        # {
+        #     "session": "NORMAL",
+        #     "duration": "GOOD_TILL_CANCEL",
+        #     "orderType": "STOP",
+        #     "complexOrderStrategyType": "NONE",
+        #     "quantity": 1200.0,
+        #     "filledQuantity": 0.0,
+        #     "remainingQuantity": 1200.0,
+        #     "requestedDestination": "AUTO",
+        #     "destinationLinkName": "AutoRoute",
+        #     "stopPrice": 13.86,
+        #     "orderLegCollection": [
+        #         {
+        #             "orderLegType": "EQUITY",
+        #             "legId": 1,
+        #             "instrument": {
+        #                 "assetType": "EQUITY",
+        #                 "cusip": "92189F817",
+        #                 "symbol": "VNM"
+        #             },
+        #             "instruction": "SELL",
+        #             "positionEffect": "CLOSING",
+        #             "quantity": 1200.0
+        #         }
+        #     ],
+        #     "orderStrategyType": "SINGLE",
+        #     "orderId": 3126389058,
+        #     "cancelable": true,
+        #     "editable": true,
+        #     "status": "QUEUED",
+        #     "enteredTime": "2020-07-30T01:08:58+0000",
+        #     "accountId": 455102033
+        # }
+        self.raw = raw
+
+    def __str__(self):
+        for leg in self.raw["orderLegCollection"]:
+            instruction = leg["instruction"]
+            quantity = int(leg["quantity"])
+            if instruction == "SELL":
+                quantity *= -1
+            symbol = leg["instrument"]["symbol"]
+            effect = leg["positionEffect"]
+
+            # return f"SELL -1,200 VNM STP 13.86 GTC [TO CLOSE]"
+            return (
+                f"{instruction} {quantity} {symbol} {self.order_type} "
+                f"{self.price} {self.duration} {effect}"
+            )
+
+    @property
+    def order_type(self):
+        return self.raw["orderType"]
+
+    @property
+    def duration(self):
+        return self.raw["duration"]
+
+    @property
+    def price(self):
+        if "stopPrice" in self.raw:
+            return self.raw["stopPrice"]
+        else:
+            return 0
+
+
+class TdAmeritrade:
     def __init__(self, account_id, api_key, token_path, redirect_uri):
         self.account_id = account_id
         try:
@@ -19,27 +134,43 @@ class TdaPortfolio:
                     driver, api_key, redirect_uri, token_path
                 )
 
-    def set_mish_stop(self, symbol, quantity, target_ratio, target, stop):
-        sell_stop_order = (
+    def get_positions(self):
+        r = self.c.get_account(self.account_id, fields=self.c.Account.Fields.POSITIONS)
+        if not r.ok:
+            raise BrokerException(r)
+
+        data = r.json()
+        account = data["securitiesAccount"]
+        positions = {}
+        for v in account["positions"]:
+            position = Position(v)
+            positions[position.symbol] = position
+        return positions
+
+    def get_position(self, symbol):
+        positions = self.get_positions()
+        if symbol in positions:
+            return positions[symbol]
+        return None
+
+    def get_order(self, order_id):
+        r = self.c.get_order(order_id, self.account_id)
+        if not r.ok:
+            raise BrokerException(r)
+
+        return Order(r.json())
+
+    def place_sell_stop(self, symbol, quantity, stop):
+        order = (
             equity_sell_market(symbol, quantity)
             .set_order_type(OrderType.STOP)
             .set_duration(Duration.GOOD_TILL_CANCEL)
             .set_stop_price(stop)
-        )
+        ).build()
 
-        sell_limit_order = equity_sell_limit(
-            symbol, quantity * target_ratio, target
-        ).set_duration(Duration.GOOD_TILL_CANCEL)
-
-        order = one_cancels_other(sell_stop_order, sell_limit_order).build()
-
-        # Currently getting this error:
-        #
-        # Conditional Orders are not permitted for accounts in this segment.
-        #
-        # Ticket opened with TDAmeritrade
         r = self.c.place_order(self.account_id, order)
-
         if not r.ok:
-            print(json.dumps(r.json(), indent=4))
-            r.raise_for_status
+            raise BrokerException(r)
+
+        order_id = Utils(self.c, self.account_id).extract_order_id(r)
+        return order_id
--- src/slamtrader/mish.py
+++ src/slamtrader/mish.py
@@ -3,12 +3,17 @@ import importlib
 import click
 
 from . import __version__
-from .brokers.tdameritrade import TdaPortfolio
+from .brokers.tdameritrade import BrokerException, TdAmeritrade
 
 
-@click.command()
+def upper(ctx, param, value):
+    return value.upper()
+
+
+@click.group()
 @click.version_option(version=__version__)
-def main():
+@click.pass_context
+def main(ctx):
     """ Semi-automate Mish's trading service """
 
     try:
@@ -16,17 +21,50 @@ def main():
     except ModuleNotFoundError:
         config = importlib.import_module("config_example")
 
-    account = TdaPortfolio(
+    ctx.obj = config
+
+
+@main.command("buy_stop")
+@click.argument("symbol", callback=upper)
+@click.argument("quantity", type=int)
+@click.argument("stop", type=float)
+@click.pass_obj
+def buy_stop(config, symbol, quantity, stop):
+    """ Buy a stock with a buy stop """
+
+    click.echo(f"Buying {quantity} share of {symbol} with buy stop at ${stop}")
+
+
+@main.command("sell_stop")
+@click.argument("symbol", callback=upper)
+@click.argument("percentage", type=float)
+@click.argument("stop", type=float)
+@click.pass_obj
+def sell_stop(config, symbol, percentage, stop):
+    """ Sell a stock with a sell stop """
+
+    broker = TdAmeritrade(
         config.tda_ira,
         config.tda_api_key,
         config.tda_token_path,
         config.tda_redirect_uri,
     )
 
-    symbol = "CGC"
-    quantity = 300
-    target_ratio = 0.5
-    stop = 15.87
-    target = 21.40
+    try:
+        position = broker.get_position(symbol)
+        if not position:
+            raise click.ClickException(f"Not position in symbol {symbol}")
+        quantity = int(round(position.long * (percentage / 100), 0))
+        click.echo(
+            (
+                f"Selling {quantity} shares ({percentage}%) of "
+                f"{symbol} with a sell stop at {stop}"
+            )
+        )
+        order_id = broker.place_sell_stop(symbol, quantity, stop)
+        click.echo(f"Submitted order ID {order_id}")
 
-    account.set_mish_stop(symbol, quantity, target_ratio, target, stop)
+        order = broker.get_order(order_id)
+        click.echo(order)
+    except BrokerException as error:
+        raise click.ClickException(error)
